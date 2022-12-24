d80de4089d6ba3dd572bae87ee959add2a8e86a4
Sezen <sezen.ibrahim3@gmail.com>
2022-12-24 15:51:04 +0200

    Work WIth Linq and XML settings

---
+++ LinqWithEFCore/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ProcessSettings();
+    }
+
+    static void ProcessSettings()
+    {
+        XDocument xml = XDocument.Load("settings.xml");
+
+        var appSettings = xml.Descendants("appSettings")
+            .Descendants("add")
+            .Select(node => new
+            {
+                Key = node.Attribute("key")?.Value,
+                Value = node.Attribute("value")?.Value
+            })
+            .ToArray();
+
+        foreach (var item in appSettings)
+        {
+            Console.WriteLine($"{item.Key} - {item.Value}");
+        }
+    }
+}
---
+++ Threads/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+Stopwatch watch = new();
+Console.Write("Press ENTER to start. ");
+Console.ReadLine();
+watch.Start();
+int max = 45;
+IEnumerable<int> numbers = Enumerable.Range(start: 1, count: max);
+Console.WriteLine($"Calculating Fibonacci sequence up to {max}. Please wait...");
+int[] fibonacciNumbers = numbers.AsParallel()
+.Select(number => Fibonacci(number))
+.OrderBy(x => x)
+.ToArray();
+watch.Stop();
+Console.WriteLine("{0:#,##0} elapsed milliseconds.",
+arg0: watch.ElapsedMilliseconds);
+Console.Write("Results:");
+foreach (int number in fibonacciNumbers)
+{
+    Console.Write($" {number}");
+}
+static int Fibonacci(int term) =>
+term switch
+{
+    1 => 0,
+    2 => 1,
+    _ => Fibonacci(term - 1) + Fibonacci(term - 2)
+};
