7636388f07eadde8dce241fec223da32163e9fe3
Sezen <sezen.ibrahim3@gmail.com>
2023-01-13 00:30:27 +0200

    Array Tasks/Reverse an Array/Palindrome

---
+++ ArraysTasksAndAlgorithms/Class1.cs
@@ -0,0 +1,92 @@
+﻿namespace ArraysTasksAndAlgorithms
+{
+    public class Class1
+    {
+        public static bool isPalRec(String str,
+                   int s,
+                   int e)
+        {
+
+            // If there is only one character
+            if (s == e)
+                return true;
+
+            // If first and last character
+            // do not match
+            if ((str[s]) != (str[e]))
+                return false;
+
+            // If there are more than two
+            // characters, check if middle
+            // substring is also
+            // palindrome or not.
+            if (s < e + 1)
+                return isPalRec(str, s + 1,
+                                e - 1);
+            return true;
+        }
+
+        public static bool isPalindrome(String str)
+        {
+            int n = str.Length;
+
+            // An empty string is considered
+            // as palindrome
+            if (n == 0)
+                return true;
+
+            return isPalRec(str, 0, n - 1);
+        }
+
+        public static string ReverseString(string input)
+        {
+            char[] arr = input.ToCharArray();
+
+            string result = string.Empty;
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                result += arr[i];
+            }
+
+            return result;
+        }
+        public static void ReverseAr(int[] arr, int end)
+        {
+            int temp;
+
+            int n = arr.Length;
+            int lastindex = arr.Length - 1;
+            int start = 0;
+
+            while (start < end)
+            {
+                temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+
+        }
+        public static void reverseIntArray(int[] arr, int input)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i += input)
+            {
+                int left = i;
+                int right = Math.Min(i + input - 1, n - 1);
+
+                int temp = 0;
+                while (left < right)
+                {
+                    temp = arr[left];
+                    arr[left] = arr[right];
+                    arr[right] = temp;
+                    left++;
+                    right--;
+                }
+            }
+        }
+    }
+}
--- WorkingWithTasks/Program.cs
+++ WorkingWithTasks/Program.cs
@@ -1,20 +1,54 @@
-﻿using System.Diagnostics; // Stopwatch
+﻿using System;
+using System.Diagnostics; // Stopwatch
 using static System.Console;
 internal class Program
 {
     private static void Main(string[] args)
     {
-        OutputThreadInfo();
-        Stopwatch timer = Stopwatch.StartNew();
+        int[] myNum = { 10, 20, 30, 40 };
+        Reverse(myNum, 2, 3);
 
-        ThreadNumbers(5, 20);
-        WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+        //OutputThreadInfo();
+        //Stopwatch timer = Stopwatch.StartNew();
+
+        //List<int> numbers = Enumerable.Range(0, 10000).ToList();
+        //for (int i = 0; i < 4; i++)
+        //{
+        //    new Thread(() =>
+        //    {
+        //        while (numbers.Count > 0)
+        //            lock (numbers)
+        //            {
+        //                if (numbers.Count == 0) break;
+        //                int lastIndex = numbers.Count - 1;
+        //                numbers.RemoveAt(lastIndex);
+        //            }
+        //    }).Start();
+        //}
+
+
+
+        //ThreadNumbers(5, 20);
+        //WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
 
         //WriteLine("Running methods synchronously on one thread.");
         //MethodA();
         //MethodB();
         //MethodC();
         //WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+    }
+    static void RectulangerArray(int a, int b)
+    {
+        var matrix = new int[a, b];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                matrix[i, j] = i * 3 + j;
+
+        foreach (var item in matrix)
+        {
+            Console.WriteLine(item);
+        }
     }
     static void ThreadNumbers(int start, int end)
     {
@@ -28,6 +62,25 @@ internal class Program
         Console.WriteLine("Thread finished work");
     }
 
+    private static void Reverse(int[] arr, int k, int s)
+    {
+        int startIndex = k;
+        int endIndex = s;
+        var result = new List<int>();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = startIndex; j < endIndex / 2; j++)
+            {
+                result.Add(arr[j]);
+            }
+        }
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
     private static void PrintEvenNumbers(int start, int end)
     {
         var result = new List<int>();
